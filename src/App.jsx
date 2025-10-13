cb0393c463d9396d9002e52d081cfcada61244c9
7Naai <iangomezchacon@gmail.com>
2025-10-13 18:34:27 -0300

    Creacion de footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,28 @@ import Nosotros from './pages/Nosotros.jsx';
 import Blog from './pages/Blog.jsx';
 import Contacto from './pages/Contacto.jsx';
 import Header from './components/Header.jsx'
+import Footer from "./components/Footer.jsx";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-  function App() {
-    return (
-        <Router>
-          <Header />
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <Header />
+        <div className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/Productos" element={<Productos />} />
             <Route path="/Nosotros" element={<Nosotros />} />
             <Route path="/Blog" element={<Blog />} />
             <Route path="/Contacto" element={<Contacto />} />
-
           </Routes>
-
-        </Router>
-      );
-    }
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function Footer() {
+    return (
+        <footer className="text-light text-center py-3" style={{backgroundColor:'#272727'}}>
+            <div className="container">
+                <small>© 2025 Heartz. Todos los derechos reservados. Av. Apoquindo 2730</small>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
