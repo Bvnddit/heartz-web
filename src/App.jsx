6bc8c705981aa24850057144d92449c9962fdd45
bvnddit <alangadiel11@gmail.com>
2025-10-14 21:41:16 -0300

    Fix rutas GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react';
-import './assets/css/main.css'
-import Home from './pages/Home.jsx';
-import Productos from './pages/Productos.jsx';
-import Nosotros from './pages/Nosotros.jsx';
-import Blog from './pages/Blog.jsx';
-import Contacto from './pages/Contacto.jsx';
-import Header from './components/Header.jsx'
-import Footer from "./components/Footer.jsx";
-import Login from './pages/Login.jsx';
-import DetalleBlog1 from './pages/DetalleBlog1.jsx';
-import DetalleBlog2 from './pages/DetalleBlog2.jsx';
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Productos from "./pages/Productos";
+import Nosotros from "./pages/Nosotros";
+import Blog from "./pages/Blog";
+import Contacto from "./pages/Contacto";
+import Login from "./pages/Login";
+import DetalleBlog1 from "./pages/DetalleBlog1";
+import DetalleBlog2 from "./pages/DetalleBlog2";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/heartz-web">
       <div className="app-container">
         <Header />
         <div className="main-content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/Productos" element={<Productos />} />
-              <Route path="/Nosotros" element={<Nosotros />} />
-              <Route path="/Blog" element={<Blog />} />
-              <Route path="/Contacto" element={<Contacto />} />
-              <Route path="/Login" element={<Login />} />
-              <Route path="/DetalleBlog1" element={<DetalleBlog1 />} />
-              <Route path="/DetalleBlog2" element={<DetalleBlog2 />} />
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Productos" element={<Productos />} />
+            <Route path="/Nosotros" element={<Nosotros />} />
+            <Route path="/Blog" element={<Blog />} />
+            <Route path="/Contacto" element={<Contacto />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/DetalleBlog1" element={<DetalleBlog1 />} />
+            <Route path="/DetalleBlog2" element={<DetalleBlog2 />} />
+          </Routes>
         </div>
         <Footer />
       </div>
