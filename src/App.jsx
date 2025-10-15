52db8ebe0f4c98b1f35728bb577e68df98ca8785
bvnddit <alangadiel11@gmail.com>
2025-10-14 21:47:49 -0300

    Corregi app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
+import './assets/css/main.css'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
