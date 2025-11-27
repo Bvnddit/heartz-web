f78aaba785f7b0c86a2e5ab8898aafc2951e8320
bvnddit <alangadiel11@gmail.com>
2025-11-27 20:55:09 -0300

    Implementación de ubicación de la tienda en el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -44,6 +44,18 @@ function Footer() {
                         <p>Las Condes, Santiago</p>
                         <p className="mt-4"><strong>Teléfono:</strong> <span>+56 9 1234 5678</span></p>
                         <p><strong>Email:</strong> <span>contacto@heartz.cl</span></p>
+                        <div className="mt-3 overflow-hidden rounded-3 shadow-sm">
+                            <iframe
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3330.2144953341353!2d-70.6013263!3d-33.4176518!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662cf6a675d731f%3A0x652a88d62abde692!2sAv.%20Apoquindo%202730%2C%207550000%20Las%20Condes%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses!2scl!4v1764286083691!5m2!1ses!2scl"
+                                width="100%"
+                                height="200"
+                                style={{ border: 0 }}
+                                allowFullScreen=""
+                                loading="lazy"
+                                referrerPolicy="no-referrer-when-downgrade"
+                                title="Ubicación Heartz"
+                            ></iframe>
+                        </div>
                     </div>
                 </div>
             </div>
